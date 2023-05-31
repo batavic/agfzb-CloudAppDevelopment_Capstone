ed7354b014b6e44c2becc992d4780972b5281990
Victoria Bata <85346019+batavic@users.noreply.github.com>
2023-05-31 17:24:22 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -26,8 +26,29 @@ class CarModel(models.Model):
 
 
 class CarDealer:
-    # Plain Python class to hold dealer data
-    pass
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
 class DealerReview:
